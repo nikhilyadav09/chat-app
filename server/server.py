f389581ceeac291a99496f8b5a2385b721eabc07
Nikhil Yadav <nikhilyadav1921@gmail.com>
2026-03-07 18:28:01 +0530

    version 2-multiconnection

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HOST = "127.0.0.1"
 PORT = 5000
@@ -8,23 +9,37 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 server_socket.bind((HOST, PORT))
-
 server_socket.listen()
 
 print(f"Server running on {HOST}:{PORT}")
 
+
+def handle_client(client_socket, address):
+    print(f"New connection from {address}")
+
+    while True:
+        try:
+            data = client_socket.recv(1024)
+
+            if not data:
+                break
+
+            message = data.decode()
+            print(f"[{address}] {message}")
+
+        except:
+            break
+
+    print(f"Client disconnected: {address}")
+    client_socket.close()
+
+
 while True:
     client_socket, address = server_socket.accept()
 
-    print(f"Client connected from {address}")
+    client_thread = threading.Thread(
+        target=handle_client,
+        args=(client_socket, address)
+    )
 
-    while True:
-        data = client_socket.recv(1024)
-
-        if not data:
-            break
-
-        message = data.decode()
-        print(f"Message received: {message}")
-
-    client_socket.close()
+    client_thread.start()
